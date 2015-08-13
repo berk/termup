c210f3f9a52071632f98249aee58e88ee54df8ef
Michael Berkovich <michael@translationexchange.com>
2015-08-12 17:27:25 -0700

    latest changes

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Automate opening up terminal tabs (or split panes) with a set of routine command
 
 It's the easiest way to get started for your projects every day.
 
-Compatible with Terminal.app, iTerm and iTerm2 on Mac OS X 10.6 - 10.8 and Ruby 1.8.7 - 1.9.3.
+Compatible with Terminal.app and iTerm 2 on OSX 10.6 - 10.9 and Ruby 1.8.7 - 2.2.
 
 ![Split Panes](https://github.com/kenn/termup/raw/master/images/split_panes.png)
+
+**For iTerm 1:** Termup 2.0 and up is not compatible with iTerm 1. Install 1.3.1 by `gem install termup -v=1.3.1`
 
 Installation
 ------------
@@ -18,6 +20,18 @@ $ gem install termup
 
 Note that you need to prepend `sudo` if you're using the OSX pre-installed Ruby.
 
+### When failed to build rb-appscript
+
+Termup has a dependency on the `rb-appscript` gem to bridge AppleScript, but `rb-appscript` is no longer maintained by the original author, and newer Ruby versions won't build it.
+
+To fix it, clone [the forked version](https://github.com/kenn/appscript), then run the following commands to install patched version of rb-appscript.
+
+```sh
+$ cd appscript/rb-appscript/trunk/
+$ gem build rb-appscript.gemspec
+$ gem install rb-appscript-0.6.1.gem
+```
+
 Usage
 -----
 
@@ -26,38 +40,38 @@ Usage
 Call the following command:
 
 ```sh
-$ termup create new_project
+$ termup create myproject
 ```
 
-This will create a new project at `~/.config/termup/new_project.yml`. Edit the file:
+This will create a new project at `~/.config/termup/myproject.yml`. Edit the file:
 
 ```sh
-$ termup edit new_project
+$ termup edit myproject
 ```
 
 And now you're good to go:
 
 ```sh
-$ termup start new_project
+$ termup start myproject
 ```
 
 ### YAML Syntax ###
 
 ```yaml
-# ~/.config/termup/new_project.yml
+# ~/.config/termup/myproject.yml
 ---
 tabs:
-  - tab1:
+  tab1:
     - cd ~/projects/foo
     - git status
-    - mate .
-  - tab2:
+    - subl .
+  tab2:
     - mysql -u root
     - show databases;
-  - tab3:
+  tab3:
     - cd ~/projects/foo
     - tail -f log/development.log
-  - tab4:
+  tab4:
     - cd ~/projects/foo
     - autotest
 options:
@@ -73,14 +87,23 @@ Tabs can contain a single command, or YAML arrays to execute multiple commands.
 Commands have a shortcut for even fewer keystrokes.
 
 ```sh
-$ termup s new_project
+$ termup s myproject
 ```
 
-That's equivalent to `termup start new_project`.
+That's equivalent to `termup start myproject`.
 
-### iTerm 2 Split Pane Support ###
+iTerm 2 Split Pane Support
+--------------------------
 
-Specify iTerm option to use split pane.
+There are two options to deal with split panes on iTerm 2: `--iterm_basic` and `--iterm_advanced`
+
+### --iterm_basic
+
+```sh
+termup create myproject --iterm_basic
+```
+
+This will generate additional options in the config file:
 
 ```yaml
 options:
@@ -89,7 +112,7 @@ options:
     height: 2
 ```
 
-The setting above turns to:
+The setting above will generate four panes in the following layout.
 
     #################
     #       #       #
@@ -101,4 +124,67 @@ The setting above turns to:
     #       #       #
     #################
 
-Enjoy!
+### --iterm_advanced
+
+```sh
+termup create myproject --iterm_advanced
+```
+
+This will generate a config file in an advanced format:
+
+```yaml
+# COMMENT OF SCRIPT HERE
+---
+tabs:
+  pane1:
+    commands:
+      - echo pane1
+    layout:
+      - split_vertically
+  pane2:
+    commands:
+      - echo pane2
+    layout:
+      - split_horizontally
+  pane3:
+    commands:
+      - echo pane3
+    layout:
+      - split_horizontally
+  pane4:
+    commands:
+      - echo pane4
+```
+
+The setting above will generate four panes in the following layout.
+
+    #################
+    #       #       #
+    #       #   2   #
+    #       #       #
+    #       #########
+    #       #       #
+    #   1   #   3   #
+    #       #       #
+    #       #########
+    #       #       #
+    #       #   4   #
+    #       #       #
+    #################
+
+Available layout commands:
+
+```ruby
+new_tab
+close_tab
+goto_previous_tab
+goto_next_tab
+goto_previous_pane
+goto_next_pane
+split_vertically
+split_horizontally
+go_left
+go_right
+go_down
+go_up
+```
--- lib/termup/base.rb
+++ lib/termup/base.rb
@@ -1,140 +1,90 @@
-require 'appscript'
 require 'yaml'
 
 module Termup
   class Base
-    ITERM1 = /^0\.10/
-    ITERM2 = /^1.0/
-
-    include Appscript
-
     def initialize(project)
-      @apps = app('System Events').application_processes
-      @frontmost = @apps.get.select{|i| i.frontmost.get }.map{|i| i.name.get }.first
-      return unless ['Terminal', 'iTerm'].include?(@frontmost)
-      @terminal = app(@frontmost)
+      @handler = Termup::Handler.new
 
-      @project = YAML.load(File.read("#{TERMUP_DIR}/#{project}.yml"))
+      config = YAML.load(File.read("#{TERMUP_DIR}/#{project}.yml"))
+      @tabs = config['tabs']
 
-      # Split panes for iTerm 2 layout with iterm commands
-      return split_panes2 if iterm2? and @project['options']['iterm2']
+      # Config file compatibility checking
+      if @tabs.is_a?(Array) and @tabs.first.is_a?(Hash)
+        abort 'YAML syntax for config has been changed. See https://github.com/kenn/termup for details.'
+      end
+
+      @options = config['options'] || {}
+      @iterm_options = @options['iterm']
 
       # Split panes for iTerm 2
-      split_panes if iterm2? and @project['options']['iterm']
+      split_panes if @handler.iterm? and @iterm_options
 
-      @project['tabs'].each do |hash|
-        tabname = hash.keys.first
-        cmds = hash.values.first
-        cmds = [cmds].flatten
-        tab = new_tab
-        cmds.each do |cmd|
-          if terminal?
-            @terminal.do_script(cmd, :in => tab)
+      # Setting up tabs / panes
+      @tabs.each_with_index do |(tabname, values), index|
+        # Set tab title
+        @handler.set_property(:name, tabname)
+
+        # Run commands
+        (advanced_iterm? ? values['commands'] : values).each do |command|
+          @handler.run command
+        end
+
+        # Layout
+        if advanced_iterm?
+          values['properties'].each do |key, value|
+            @handler.set_property(key, value)
+          end if values['properties']
+
+          values['layout'].each do |command|
+            layout command
+          end if values['layout']
+        else
+          # Move to next
+          if @iterm_options
+            layout :goto_next_pane
           else
-            @terminal.current_terminal.current_session.write(:text => "#{cmd}")
+            if index < @tabs.size - 1
+              layout :new_tab
+              sleep 0.01 # Allow some time to complete opening a new tab
+            else
+              layout :goto_next_tab # Back home
+            end
           end
         end
       end
-    end
-
-    def new_tab
-      if @got_first_tab_already
-        if iterm2?
-          @apps[@frontmost].keystroke(']', :using => :command_down)
-        else
-          @apps[@frontmost].keystroke('t', :using => :command_down)
-        end
-      end
-      @got_first_tab_already = true
-      sleep 0.01 # Allow some time to complete opening a new tab
-      @terminal.windows[1].tabs.last.get if terminal?
     end
 
     def split_panes
-      # Virtical splits
-      (@project['options']['iterm']['width'] - 1).times do |i|
-        @apps[@frontmost].keystroke('d', :using => :command_down)
+      width, height = @iterm_options['width'], @iterm_options['height']
+      return unless width and height
+
+      (width - 1).times do
+        layout :split_vertically
       end
-      # Back to home
-      @apps[@frontmost].keystroke(']', :using => :command_down)
-      # Horizontal splits
-      @project['options']['iterm']['width'].times do |i|
-        (@project['options']['iterm']['height'] - 1).times do |i|
-          @apps[@frontmost].keystroke('d', :using => [ :command_down, :shift_down ])
+      layout :goto_next_pane # Back home
+      width.times do
+        (height - 1).times do
+          layout :split_horizontally
         end
-        # Move to the right
-        @apps[@frontmost].keystroke(']', :using => :command_down)
-      end
-    end
-
-    def split_panes2
-      @project['tabs'].each_with_index do |hash, index|
-        tabname = hash.keys.first
-        tab = hash[tabname]
-        @terminal.current_terminal.current_session.name.set(tabname)
-
-        if tab['background']
-          @terminal.current_terminal.current_session.background_color.set(tab['background'])
-        end
-
-        if tab['foreground']
-          @terminal.current_terminal.current_session.foreground_color.set(tab['foreground'])
-        end
-
-        if tab['transparency']
-          @terminal.current_terminal.current_session.transparency.set(tab['transparency'])
-        end
-
-        tab['commands'].each do |cmd|
-          @terminal.current_terminal.current_session.write(:text => "#{cmd}")
-        end
-
-        tab['layout'] ||= []
-        if tab['layout'].empty? and index < @project['tabs'].size - 1
-          tab['layout'] << 'new_tab' 
-        end
-
-        tab['layout'].each do |cmd|
-          case cmd
-          when 'new_tab'
-            @apps[@frontmost].keystroke('t', :using => [ :command_down ] )
-          when 'close_tab'
-            @apps[@frontmost].keystroke('w', :using => [ :command_down ] )
-          when 'go_to_previous_tab'
-            @apps[@frontmost].key_code(123, :using => [ :command_down ] )
-          when 'go_to_next_tab'
-            @apps[@frontmost].key_code(124, :using => [ :command_down ] )
-          when 'split_horizontally'
-            @apps[@frontmost].keystroke('d', :using => [ :command_down, :shift_down ] )
-          when 'split_vertically'
-            @apps[@frontmost].keystroke('d', :using => [ :command_down ] )
-          when 'go_left'
-            @apps[@frontmost].key_code(123, :using => [ :command_down, :option_down ] )
-          when 'go_right'
-            @apps[@frontmost].key_code(124, :using => [ :command_down, :option_down ] )
-          when 'go_down'
-            @apps[@frontmost].key_code(125, :using => [ :command_down, :option_down ] )
-          when 'go_up'
-            @apps[@frontmost].key_code(126, :using => [ :command_down, :option_down ] )
-          else
-            raise "Unknown iTerm2 command #{cmd}"
-          end
-
-          sleep 0.01
-        end
+        layout :goto_next_pane # Move to next, or back home
       end
     end
 
-    def terminal?
-      @frontmost == 'Terminal'
+    def advanced_iterm?
+      unless defined?(@advanced_iterm)
+        @advanced_iterm = case @tabs.values.first
+        when Hash   then true
+        when Array  then false
+        else
+          abort 'invalid YAML format'
+        end
+        abort 'advanced config only supported for iTerm' if @advanced_iterm and !@handler.iterm?
+      end
+      @advanced_iterm
     end
 
-    def iterm1?
-      @frontmost == 'iTerm' and @terminal.version.get =~ ITERM1
-    end
-
-    def iterm2?
-      @frontmost == 'iTerm' and @terminal.version.get =~ ITERM2
+    def layout(command)
+      @handler.layout(command)
     end
   end
 end
